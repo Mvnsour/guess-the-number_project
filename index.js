8cad2183382143903f523080f7d13a00daaf168e
Mansour Mbengue <mbgmansour13@gmail.com>
2024-11-22 05:27:26 +0100

    first step : create a const which generate a random number  between 0 and 100

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 import { prompt } from "./prompt.js";
 
-const test = Number(prompt('Quel âge as tu ? : '));
-console.log(test, "ans.");
+console.log(
+`Welcome to the Number Guessing Game! 🎮
+
+Rules:
+1. The system will generate a random number between 0 and 100.
+2. Your task is to guess this number.
+3. Enter a number when prompted.
+4. If your guess is too high or too low, you'll be notified, and you can guess again.
+5. The game continues until you guess the correct number.
+
+Let's get started! 🚀`
+);
+
+let targetNumber = Math.floor(Math.random() * 100); //random number generated between 0 and 100, Math.floor() is used to round the number and get a whole number
+console.log(targetNumber);  // to see the result
