805951819e8db1fdf458b095eb78d7510df35d21
Mansour Mbengue <mbgmansour13@gmail.com>
2024-12-04 07:27:53 +0100

    new function named "game" which has his own operation. The goal is to work with recursive function. And now we can play this game each time

--- index.js
+++ index.js
@@ -1,5 +1,57 @@
 import { prompt } from "./prompt.js";
 
+function isValidNumber(number) {
+  // check if the number entered is really a number and if it's a number between 0 and 100
+    return !Number.isNaN(number) && number >= 1 && number <= 100;
+  }
+
+function game() {
+  let targetNumber = Math.floor(Math.random() * 100); //random number generated between 0 and 100, Math.floor() is used to round the number and get a whole number
+  let attemptCount = 0; 
+
+  const guessingGame = () => {
+    const userInput = Number(prompt('Enter a number : '));
+    attemptCount++; // incrementation of attempt atfer the user input
+  
+    if (!isValidNumber(userInput)) { // using "!" means negation 
+      console.log(`${userInput} is an invalid number. It must be a number between 0 and 100.\n\n`);
+      guessingGame(); // calling the func to ask again the user, recursive function
+      return; // to stop the script
+    }
+  
+    if (userInput < targetNumber) {
+      console.log(`The number entered "${userInput}" is too small. Try again.\n`);
+      guessingGame(); 
+    }
+  
+    if (userInput > targetNumber) {
+      console.log(`The number entered "${userInput}" is too big. Try again.\n`);
+      guessingGame(); 
+    }
+  // correct answer found by the user 
+      console.log(`Congratulations ! ${userInput} was the random number.`);
+      console.log(`You tried ${attemptCount} times.\n\n`);
+  }
+
+  const replayGame = () => {
+    const choice = prompt("Do you want to play again ? (y/n) : ")
+  
+    if (choice === "y") {
+      console.log("Here we go again !\n\n");
+      guessingGame();
+    } else if (choice === "n") {
+      console.log("Thank you for playing ! Goodbye");
+      return;
+    } else {
+        console.log(`I don't understand your choice. Please, choose between "y" and "n".\n`);
+        replayGame();
+      }
+    }
+
+    guessingGame();
+    replayGame();
+}
+
 console.log(
 `Welcome to the Number Guessing Game! 🎮
 
@@ -13,58 +65,4 @@ Rules:
 Let's get started! 🚀`
 );
 
-let targetNumber = Math.floor(Math.random() * 100); //random number generated between 0 and 100, Math.floor() is used to round the number and get a whole number
-let attemptCount = 0; 
-console.log(targetNumber);  // to see the result
-
-function isValidNumber(number) {
-// check if the number entered is really a number and if it's a number between 0 and 100
-  return !Number.isNaN(number) && number >= 1 && number <= 100;
-}
-
-function guessingGame() {
-  const userInput = Number(prompt('Enter a number : '));
-  attemptCount++; // incrementation of attempt atfer the user input
-
-  if (!isValidNumber(userInput)) { // using "!" means negation 
-    console.log(`${userInput} is an invalid number. It must be a number between 0 and 100.\n\n`);
-    guessingGame(); // calling the func to ask again the user, recursive function
-    return; // to stop the script
-  }
-
-  if (userInput < targetNumber) {
-    console.log(`The number entered "${userInput}" is too small. Try again.\n`);
-    guessingGame(); 
-  }
-
-  if (userInput > targetNumber) {
-    console.log(`The number entered "${userInput}" is too big. Try again.\n`);
-    guessingGame(); 
-  }
-
-  if (userInput === targetNumber) {
-
-/* we can also do without condition because it is the last option after previous checks */ 
-
-    console.log(`Congratulations ! ${userInput} was the random number.`);
-    console.log(`You tried ${attemptCount} times.\n\n`);
-  }
-}
-
-function replayGame() {
-const choice = prompt("Do you want to play again ? (y/n) : ")
-
-  if (choice === "y") {
-    console.log("Here we go again !\n\n");
-    guessingGame();
-  } else if (choice === "n") {
-    console.log("Thank you for playing ! Goodbye");
-    return;
-  } else {
-      console.log(`I don't understand your choice. Please, choose between "y" and "n".\n`);
-      replayGame();
-    }
-  }
-
-  guessingGame();
-  replayGame();
+game();
